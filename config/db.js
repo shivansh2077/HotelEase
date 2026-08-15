91e2cdcd5498fa1d96628fe39ab25f385f71d686
shivansh2077 <singhshivansh101@gmail.com>
2026-08-15 19:08:25 +0530

    Refactor DB connection to support base64 SSL CA
    Updated SSL configuration to use base64 encoded CA.

--- config/db.js
+++ config/db.js
@@ -1,28 +1,16 @@
-// const mysql = require("mysql2/promise");
-// require("dotenv").config();
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10
-// });
-
-// module.exports = pool;
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 let sslConfig;
 
 if (process.env.DB_SSL === "true") {
-  if (!process.env.DB_SSL_CA) {
-    throw new Error("DB_SSL_CA is required when DB_SSL=true.");
+  if (!process.env.DB_SSL_CA_BASE64) {
+    throw new Error("DB_SSL_CA_BASE64 is required when DB_SSL=true.");
   }
 
   sslConfig = {
-    ca: process.env.DB_SSL_CA.replace(/\\n/g, "\n"),
+    ca: Buffer.from(process.env.DB_SSL_CA_BASE64, "base64").toString("utf8"),
     rejectUnauthorized: true
   };
 }
@@ -39,3 +27,45 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
+
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10
+// });
+
+// module.exports = pool;
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
+
+// let sslConfig;
+
+// if (process.env.DB_SSL === "true") {
+//   if (!process.env.DB_SSL_CA) {
+//     throw new Error("DB_SSL_CA is required when DB_SSL=true.");
+//   }
+
+//   sslConfig = {
+//     ca: process.env.DB_SSL_CA.replace(/\\n/g, "\n"),
+//     rejectUnauthorized: true
+//   };
+// }
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT || 3306),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   ssl: sslConfig,
+//   waitForConnections: true,
+//   connectionLimit: 5
+// });
+
+// module.exports = pool;
